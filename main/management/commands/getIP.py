b9753a6b4cea2fa146fb6673c26f072d64fbf289
aokly <aoklyunin@gmail.com>
2020-07-02 22:22:09 +0300

    get ip

--- main/management/commands/getIP.py
+++ main/management/commands/getIP.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 import wikipedia as wikipedia
 from django.contrib.auth.models import User
+from requests import get
 
 from main.models import Paper
 ## importing socket module
@@ -17,9 +18,6 @@ class Command(BaseCommand):
     helf = 'remove loaded textes from wiki'
 
     def handle(self, *args, **options):
-        ## getting the hostname by socket.gethostname() method
-        hostname = socket.gethostname()
-        ## getting the IP address using socket.gethostbyname() method
-        ip_address = socket.gethostbyname(hostname)
-        print(ip_address)
+        ip = get('https://api.ipify.org').text
+        print('My public IP address is:', ip)
 
